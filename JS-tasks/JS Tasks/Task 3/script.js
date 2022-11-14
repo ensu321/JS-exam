b4ec75289fb7fd29ef0d0e6727684db41961338d
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-11-14 21:55:29 +0200

    task 3 to be completed

--- JS-tasks/JS Tasks/Task 3/script.js
+++ JS-tasks/JS Tasks/Task 3/script.js
@@ -11,4 +11,35 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+fetch(ENDPOINT)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response.statusText);
+    }
+  })
+  .then((data) => {
+    renderUsers(data);
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+function renderUsers(data) {
+  const getOutPutContainer = document.getElementById("output");
+  const getMessageDiv = document.getElementById("message");
+  getMessageDiv.remove(); //removes message node
+
+  const createList = document.createElement("ul");
+  getOutPutContainer.append(createList); //create list
+
+  data.forEach((element) => {
+    const createListItem = document.createElement("li");
+    createListItem.innerText = element.login;
+    createList.append(createListItem); //create list item for each element
+  });
+}
